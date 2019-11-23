b317f27ce7b35e0dcff5cbc95f4536e3878b6098
孔勇 <kongyong@dianwoda.com>
2019-11-23 11:07:52 +0800

    first commit

---
+++ src/components/index.vue
@@ -0,0 +1,278 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-header style="font-size: 12px">
+      <el-row :gutter="20">
+        <el-col :span="4" style="text-align: center"><span><b>Account Platform</b></span></el-col>
+        <el-col :span="20" style="text-align: right">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>会灰翔的灰机</span>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['2']" default-active="1">
+          <el-menu-item index="1" v-model="menuItemName">
+            <i class="el-icon-menu"></i>
+            <span slot="title" @click="addTab(menuItemName)">{{menuItemName}}</span>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>账单</template>
+            <el-menu-item-group>
+              <template slot="title">贷款</template>
+              <el-menu-item index="2-1">房贷</el-menu-item>
+              <el-menu-item index="2-2">消费贷</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <div style="margin-bottom: 1px;">
+        </div>
+        <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab">
+          <el-tab-pane
+              v-for="(item) in editableTabs"
+              :key="item.name"
+              :label="item.title"
+              :name="item.name"
+          >
+          </el-tab-pane>
+        </el-tabs>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-select
+                v-model="value"
+                multiple
+                filterable
+                default-first-option
+                placeholder="请选择标签">
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+              </el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="12" style="text-align: right;">
+            <el-button icon="el-icon-search" circle></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle style="margin-right: 20px;"></el-button>
+          </el-col>
+        </el-row>
+        <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            height="380"
+            border
+            style="width: 100%; margin-bottom: 10px; margin-top: 15px;"
+            :default-sort = "{prop: 'date', order: 'descending'}"
+            @selection-change="handleSelectionChange">
+          <el-table-column
+              type="selection"
+              width="55">
+          </el-table-column>
+          <el-table-column
+              fixed
+              label="日期"
+              width="150"
+              sortable>
+            <template slot-scope="scope">{{ scope.row.date }}</template>
+          </el-table-column>
+          <el-table-column
+              prop="name"
+              label="姓名"
+              width="120"
+              sortable>
+          </el-table-column>
+          <el-table-column
+              prop="province"
+              label="省份"
+              width="120">
+          </el-table-column>
+          <el-table-column
+              prop="city"
+              label="市区"
+              width="120">
+          </el-table-column>
+          <el-table-column
+              prop="address"
+              label="地址"
+              width="300"
+              show-overflow-tooltip>
+          </el-table-column>
+          <el-table-column
+              prop="zip"
+              label="邮编"
+              width="120">
+          </el-table-column>
+          <el-table-column
+              fixed="right"
+              label="操作"
+              width="120">
+            <template slot-scope="scope">
+              <el-button @click="queryDetail(scope.row)" type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="23" style="text-align:right">
+        </el-pagination>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎2',
+          address: '上海市普陀区金沙江路上海市普陀区金沙江路上海市普陀区金沙江路上海市普陀区金沙江路上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎4',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎2',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎4',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎2',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎4',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        editableTabsValue: '1',
+        menuItemName: '概览',
+        editableTabs: [{
+          title: '概览',
+          name: '1',
+          content: 'Tab 1 content'
+        }],
+        tabIndex: 2,
+        multipleSelection: [],
+        options: [{
+          value: '1',
+          label: 'HTML'
+        }, {
+          value: '2',
+          label: 'CSS'
+        }, {
+          value: '3',
+          label: 'JavaScript'
+        }],
+        value: []
+      };
+    },
+    methods: {
+      addTab(targetName) {
+        let exist = false;
+        this.editableTabs.forEach((editableTab) => {
+            if (editableTab.title === targetName) {
+              exist = true;
+            }
+          }
+        );
+        if (exist) {
+          return;
+        }
+        let newTabName = ++this.tabIndex + '';
+        this.editableTabs.push({
+          title: targetName,
+          name: newTabName,
+          content: 'New Tab content'
+        });
+        this.editableTabsValue = newTabName;
+      },
+      removeTab(targetName) {
+        let tabs = this.editableTabs;
+        let activeName = this.editableTabsValue;
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1];
+              if (nextTab) {
+                activeName = nextTab.name;
+              }
+            }
+          });
+        }
+
+        this.editableTabsValue = activeName;
+        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+      },
+      queryDetail(row) {
+        console.log(row);
+      },
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+        console.log(this.multipleSelection);
+      }
+    }
+  };
+</script>
+
+<style>
+  .el-header {
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
